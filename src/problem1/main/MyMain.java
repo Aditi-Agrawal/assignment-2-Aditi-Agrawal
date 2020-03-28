63f2d4f18cc5756366af736da509a1a845cfe34e
Aditi Agrawal <aditi.agrawal_cs18gla.ac.in>
2020-03-28 18:56:07 +0530

    completed

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,19 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(15);
+        obj.insert(10);
+        obj.insert(50);
+        obj.insert(55);
+        obj.insert(60);
+        System.out.println("left nodes of bst:");
+        obj.traverse();
+        System.out.println("count of nodes not having left node");
+        System.out.println(obj.getCountRight());
+    }
 }
--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,26 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+import problem3.student.Student;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue queue = new MyPriorityQueue();
+        queue.enqueue(new Student("M", 26));
+        queue.enqueue(new Student("A", 27));
+        queue.enqueue(new Student("P", 65));
+        queue.enqueue(new Student("S", 1));
+        queue.enqueue(new Student("V", 10));
+        queue.enqueue(new Student("Am", 76));
+
+        queue.show();
+        queue.dequeue();
+        queue.show();
+
+    }
+
 }
